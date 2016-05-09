4ed1a38c1f251e1092559ea4ce6205b1a28edd56
thruthesky <thruthesky@gmail.com>
2016-05-09 20:21:36 +0800

    work for video center server

---
+++ s.js
@@ -0,0 +1,46 @@
+
+var https = require('https');
+var express = require('express');
+var fs = require('fs');
+var path = require('path');
+var app = express();
+
+var options = {
+    key: fs.readFileSync(path.join(__dirname, 'ssl/withcenter.key')),
+    cert: fs.readFileSync(path.join(__dirname, 'ssl/withcenter.crt'))
+};
+var server = https.createServer(options, app);
+
+server.listen(1234);
+
+app.get('/', express.static('www') );
+
+app.use('/', express.static(__dirname + '/www'));
+app.use('/music', express.static(__dirname + '/music'));
+app.use('/private', express.static(__dirname + '/private'));
+
+
+
+require('./Signaling-Server.js')(app, function(socket) {
+    try {
+        var params = socket.handshake.query;
+
+        // "socket" object is totally in your own hands!
+        // do whatever you want!
+
+        // in your HTML page, you can access socket as following:
+        // connection.socketCustomEvent = 'custom-message';
+        // var socket = connection.getSocket();
+        // socket.emit(connection.socketCustomEvent, { test: true });
+
+        if (!params.socketCustomEvent) {
+            params.socketCustomEvent = 'custom-message';
+        }
+
+        socket.on(params.socketCustomEvent, function(message) {
+            try {
+                socket.broadcast.emit(params.socketCustomEvent, message);
+            } catch (e) {}
+        });
+    } catch (e) {}
+});
---
+++ s2.js
@@ -0,0 +1,73 @@
+
+var https  = require( 'https' ),
+    url     = require( 'url' ),
+    path    = require( 'path' ),
+    fs      = require( 'fs' );
+
+var options = {
+    key: fs.readFileSync(path.join(__dirname, 'ssl/withcenter.key')),
+    cert: fs.readFileSync(path.join(__dirname, 'ssl/withcenter.crt'))
+};
+
+function serverHandler(request, response) {
+    var uri = url.parse(request.url).pathname,
+        filename = path.join(process.cwd(), uri);
+
+    var stats;
+
+    try {
+        stats = fs.lstatSync(filename);
+    } catch (e) {
+        response.writeHead(404, {
+            'Content-Type': 'text/plain'
+        });
+        response.write('404 Not Found: ' + path.join('/', uri) + '\n');
+        response.end();
+        return;
+    }
+
+    if (fs.statSync(filename).isDirectory()) {
+        filename += '/index.html';
+    }
+
+    fs.readFile(filename, 'binary', function(err, file) {
+        if (err) {
+            response.writeHead(500, {
+                'Content-Type': 'text/plain'
+            });
+            response.write('404 Not Found: ' + path.join('/', uri) + '\n');
+            response.end();
+            return;
+        }
+        response.writeHead(200);
+        response.write(file, 'binary');
+        response.end();
+    });
+}
+var app = https.createServer(options, serverHandler);
+
+app.listen(10443);
+
+require('./Signaling-Server.js')(app, function(socket) {
+    try {
+        var params = socket.handshake.query;
+
+        // "socket" object is totally in your own hands!
+        // do whatever you want!
+
+        // in your HTML page, you can access socket as following:
+        // connection.socketCustomEvent = 'custom-message';
+        // var socket = connection.getSocket();
+        // socket.emit(connection.socketCustomEvent, { test: true });
+
+        if (!params.socketCustomEvent) {
+            params.socketCustomEvent = 'custom-message';
+        }
+
+        socket.on(params.socketCustomEvent, function(message) {
+            try {
+                socket.broadcast.emit(params.socketCustomEvent, message);
+            } catch (e) {}
+        });
+    } catch (e) {}
+});
